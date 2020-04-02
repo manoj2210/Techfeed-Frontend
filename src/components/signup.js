93830f5de5e39d8723701d611106b2c751ae6af2
ananthu-16 <raamanantha.16@gmail.com>
2020-04-02 17:07:03 +0530

    commiting signup page

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Layout from '../components/layout';
 
+
+
 function Signup(){
-  return <div className="mx-auto px-4 sm:px-8 min-h-screen">
+  const initialValues={
+    firstName:'',
+    lastName:'',
+    rollno:'',
+    class:'',
+    dept:'',
+    college:'',
+    email:'',
+    phno:'',
+    pswd:'',
+    cnfrmPswd:''
+  };
+
+  const [inputs,setInputs]=useState(initialValues);
+
+  function handleInputChange(event){
+    // console.log(event.target.name);
+     event.persist();
+    setInputs({
+      ...inputs,[event.target.name]: event.target.value
+    });
+  }
+  return <div className="m-auto px-4 sm:px-8 min-h-screen text-center">
   <div>
-     <h2 className="text-xl leading-tight text-gray-900">Signup</h2>
+     <h2 className="text-xl leading-tight text-gray-900 mb-5">Welcome to Techfeed</h2>
   </div>
-  <form className="bg-white shadow-md rounded px-8 py-6 pb-8 my-4">
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-16 md:w-64 px-3 mb-6 md:mb-0">
+  <div className="bg-white shadow-md rounded px-8 py-6 pb-8 text-center">
+  <form >
+  <div className="md:flex md:flex-wrap-mx-3 mb-6 justify-center block">
+
+    <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
       <label
        className="block text-gray-700 text-sm font-bold mb-2"
         htmlFor="firstname">
@@ -17,91 +43,158 @@ function Signup(){
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
        id="firstname"
+       name="firstName"
+       onChange={handleInputChange}
        type="text"/>
-      <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
     </div>
 
-    <div class="w-full md:w-64 px-3">
+    <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
       <label
       className="block text-gray-700 text-sm font-bold mb-2"
-      htmlfor="lastname" >
+      htmlFor="lastname" >
         Last Name
       </label>
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       id="lastname"
+      name="lastName"
+      onChange={handleInputChange}
       type="text" />
     </div>
   </div>
 
-  <div class="flex flex-wrap -mx-3 mb-6 w-full md:w-64">
+  <div className="md:flex md:flex-wrap-mx-3 mb-6 justify-center block">
 
-  <div class="w-full ms:w-1/2 px-3">
+  <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
     <label
     className="block text-gray-700 text-sm font-bold mb-2"
-    htmlfor="rollno" >
-      Rollno
+    htmlFor="rollno" >
+      Roll number
     </label>
     <input
       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       id="rollno"
+      name="rollno"
+      onChange={handleInputChange}
     type="text" />
    </div>
-
-    <div class="w-full px-3">
-      <label
-      className="block text-gray-700 text-sm font-bold mb-2"
-      htmlfor="password" >
-        Password
-      </label>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="password"
-      type="password" />
-      <p class="text-gray-600 text-xs italic">Must be 8 characters long</p>
+   <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+     <label
+     className="block text-gray-700 text-sm font-bold mb-2"
+     htmlFor="class" >
+       Class
+     </label>
+     <input
+       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+       id="class"
+       name="class"
+       onChange={handleInputChange}
+     type="text" />
     </div>
+
   </div>
+  <div className="md:flex md:flex-wrap-mx-3 mb-6 justify-center block">
 
-  <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-64 px-3 mb-6 md:mb-0">
+  <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+    <label
+    className="block text-gray-700 text-sm font-bold mb-2"
+    htmlFor="dept" >
+      Department
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="dept"
+      name="dept"
+      onChange={handleInputChange}
+    type="text" />
+   </div>
+   <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+     <label
+     className="block text-gray-700 text-sm font-bold mb-2"
+     htmlFor="college" >
+       College
+     </label>
+     <input
+       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+       id="college"
+       name="college"
+       onChange={handleInputChange}
+     type="text" />
+    </div>
+
+  </div>
+  <div className="md:flex md:flex-wrap-mx-3 mb-6 justify-center block">
+
+  <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+    <label
+    className="block text-gray-700 text-sm font-bold mb-2"
+    htmlFor="email" >
+      Email
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="email"
+      name="email"
+      onChange={handleInputChange}
+    type="email" />
+   </div>
+   <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+     <label
+     className="block text-gray-700 text-sm font-bold mb-2"
+     htmlFor="phno" >
+       Phone number
+     </label>
+     <input
+       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+       id="phno"
+       onChange={handleInputChange}
+       name="phno"
+       pattern="[0-9]{10}" required
+     type="tel" />
+    </div>
+
+  </div>
+  <div className="md:flex md:flex-wrap-mx-3 mb-6 justify-center block">
+  <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
+    <label
+    className="block text-gray-700 text-sm font-bold mb-2"
+    htmlFor="password" >
+      Password
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="password"
+      onChange={handleInputChange}
+      name="pswd"
+    type="password" />
+    <p className="text-gray-600 text-xs italic">Must be 8 characters long</p>
+    </div>
+    <div className="w-64 px-3 mb-6 md:mb-0 mx-auto sm:mx-auto md:mx-0">
       <label
       className="block text-gray-700 text-sm font-bold mb-2"
-      for="grid-city">
-        City
+      htmlFor="Confirmpassword" >
+        Confirm Password
       </label>
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="grid-city"
-      type="text" />
-    </div>
-    <div class="w-full md:w-64 px-3 mb-6 md:mb-0">
-      <label
-      className="block text-gray-700 text-sm font-bold mb-2"
-      for="grid-state">
-        State
-      </label>
-      <div class="relative">
-        <select
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-        </div>
-      </div>
-    </div>
-    <div class="w-full md:w-64 px-3 mb-6 md:mb-0">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="grid-zip">
-        Zip
-      </label>
-      <input   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-       id="grid-zip" type="text"  />
-    </div>
+        id="Confirmpassword"
+        name="cnfrmPswd"
+        onChange={handleInputChange}
+      type="password" />
+      <p className="text-gray-600 text-xs italic">Must be 8 characters long</p>
+  </div>
+  </div>
+  <div>
+  <button
+      className="bg-orange-500 hover:bg-orange-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="submit"
+  >
+     Sign Up
+  </button>
   </div>
 </form>
+</div>
 </div>
 }
 
